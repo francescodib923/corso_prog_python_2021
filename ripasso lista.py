099cb9617954f0dfaa70062bf3486c8325bcccec
francescodib923 <francesco.dib923@gmail.com>
2021-11-25 14:32:12 +0100

    ripasso liste e dizionari

---
+++ ripasso lista.py
@@ -0,0 +1,20 @@
+list = ["a","b","c","d","e"]
+print (list[0:5])
+list2 = ["f","g","h"]
+print (list + list2)
+list.append ("j")
+print (list)
+list[1] = "cocco"
+print (list)
+print (type(list))
+
+dictionary = {"a" : 1, "b" : 2, "c" : 3}
+print (dictionary)
+print (dictionary["a"])
+print ("a" in dictionary)
+print (dictionary.items())
+print (dictionary.items())
+print (len (dictionary))
+
+
+
